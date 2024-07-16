e4c2064c1d33e3ff55ae8066c98145081bcf453c
Omar Soto <ojsg140789@gmail.com>
2024-07-16 08:54:39 -0600

    feature: Se agrega carga lazy load

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
+    {
+        path: 'home',
+        loadComponent: () => 
+            import('./components/pages/home/home.component')
+                .then(m => m.HomeComponent)
+    },
     {
         path: 'tasks/:id',
         loadComponent: () => 
