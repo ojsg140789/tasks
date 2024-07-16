06aada5e0712f031fdb3e2b4d15ef25cb828ee18
Omar Soto <ojsg140789@gmail.com>
2024-07-16 00:13:57 -0600

    feature: Se agrega carga de compoente lazy load

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
-import { TasksComponent } from './components/pages/tasks/tasks.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'tasks/:id', component: TasksComponent },
+    {
+        path: 'tasks/:id',
+        loadComponent: () => 
+            import('./components/pages/tasks/tasks.component')
+                .then(m => m.TasksComponent)
+    },
     { path: '',   redirectTo: '/home', pathMatch: 'full' },
     { path: '**', component: HomeComponent }
 ];
